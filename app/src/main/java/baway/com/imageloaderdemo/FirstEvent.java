d6357ad94dbfd4c39a6a8fd4abad8e05d7af5367
jiaxiukun <453766331@qq.com>
2017-11-02 10:13:27 +0800

    Initial commit

---
+++ app/src/main/java/baway/com/imageloaderdemo/FirstEvent.java
@@ -0,0 +1,15 @@
+package baway.com.imageloaderdemo;
+
+/**
+ * Created by 贾秀坤 on 2017/11/1.
+ */
+
+public class FirstEvent {
+    private String mMsg;
+    public FirstEvent(String msg){
+        this.mMsg=msg;
+    }
+    public String getMsg(){
+        return mMsg;
+    }
+}
---
+++ app/src/main/java/baway/com/imageloaderdemo/MainActivity.java
@@ -0,0 +1,54 @@
+package baway.com.imageloaderdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+public class MainActivity extends Activity {
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //注册EventBus
+        EventBus.getDefault().register(this);
+        btn = (Button) findViewById(R.id.btn_try);
+        tv = (TextView) findViewById(R.id.tv);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(getApplicationContext(),
+                        SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+   @Subscribe
+    public void onEventMainThread(FirstEvent event) {
+
+        String msg = "onEventMainThread收到了消息：" + event.getMsg();
+        Log.d("harvic", msg);
+        tv.setText(msg);
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);//反注册EventBus
+    }
+}
